4bfdf26eab40daaeb852d591bbe24e80949852da
miguelnkono <nkonondememiguel7@gmail.com>
2026-03-12 17:42:59 +0100

    completed the program

--- src/main.rs
+++ src/main.rs
@@ -10,25 +10,30 @@ fn main() {
     println!("Guessing the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is {secret_number}");
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        // match expression
+        // very important first use trim() here;
+        let guess: u32 = match guess.trim().parse() {
+                                    Ok(num) => num,
+                                    Err(_) => continue,
+                                };
 
-    let guess: u32 = guess.trim()       // very important first use trim() here;
-                            .parse()
-                            .expect("Should have put a number");
-
-    println!("You guessed: {guess}");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Equal => println!("You win!"),
-        Ordering::Greater => println!("Too big!"),
+        println!("You guessed: {guess}");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
