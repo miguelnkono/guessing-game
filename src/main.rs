a05748a217b2caa08813a60fd745c3a02d74b5ff
miguelnkono <nkonondememiguel7@gmail.com>
2026-03-12 16:57:42 +0100

    added the ability to generate random numbers

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use std::io;
 
+use rand::Rng;
+
+// we can view the documentation for all the dependencies we're using with the command:
+//      cargo doc --open
+
 fn main() {
     println!("Guessing the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is {secret_number}");
 
     println!("Please input your guess.");
 
